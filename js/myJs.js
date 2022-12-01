45e7b04eef76498f660ada291e46a7896fb49931
me <86767723+Minh311@users.noreply.github.com>
2022-12-01 12:49:33 +0700

    Update myJs.js
    Signed-off-by: me <86767723+Minh311@users.noreply.github.com>

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "Untitled-6.html";
+            window.location = "https://cuongnobro.github.io/Baby/";
           },
         });
       }
